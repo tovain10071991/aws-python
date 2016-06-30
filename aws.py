b2bec8b3340a484c0312f9c9ee4f28a453bc651b
skyin <tovain10071991@gmail.com>
2016-06-30 10:42:48 +0800

    record progress; limit memory cache of compile_unit, invoke recent queue(deque); save/read ast file; judge .inc file; add UNSUPPORTTED kind; add some case in parse_cursor

--- aws.py
+++ aws.py
@@ -17,7 +17,10 @@ elf_helper = AwsElf(executable)
 parse_helper = AwsParse()
 dis_helper = AwsDis()
 
+total = 5250
+
 if __name__ == '__main__':
+  num = 0
   offset = elf_helper.text_sec.start_off
   address = elf_helper.text_sec.start_addr
   while(1):
@@ -29,12 +32,17 @@ if __name__ == '__main__':
     if inst is None:
       break
     if(dis_helper.is_indirect_branch(inst)):
-      output = "0x%x\n" % inst.address
+      num = num + 1
+      output = "the num of parsing: %d total: %d\n" % (num, total)
+      sys.stderr.write(output)
+      output = "0x%x %s %s\n" % (inst.address, inst.mnemonic, inst.op_str)
       sys.stderr.write(output)
       # sys.stderr.flush()
       file_name, line, column = dbg_helper.get_location(inst.address)
       if file_name is not None:
         if parse_helper.parse_indirect_branch(file_name, line, column) is False:
+          sys.stderr.write('indirect branch kind: IndirectBrKind.UNKNOWN\n')
+          print "indirect branch kind: IndirectBrKind.UNKNOWN"
           print("0x%x %s %s" %(inst.address, inst.mnemonic, inst.op_str))
           dbg_helper.print_source_code(inst.address)
           print "%s: %d, %d" % (file_name, line, column)
--- awsparse.py
+++ awsparse.py
@@ -1,4 +1,4 @@
-import sys, os, enum, pdb
+import sys, os, enum, pdb, collections
 sys.path.append('/home/user/Documents/llvm-3.4/tools/clang/bindings/python/')
 import clang.cindex
 from clangargs import clang_args, h_args
@@ -39,10 +39,39 @@ def get_next_column(file_name, line, column):
   return column
 
 compile_unit = {}
+recent_unit = collections.deque()
+ast_cache_dir = '/mnt/sdb/test/analysis_indirect_branch_compare_with_source/ast/'
+
+def convert_to_ast(file_name):
+  list = file_name.split('/')
+  ast_file = ast_cache_dir + 'ast'
+  for ele in list:
+    ast_file = ast_file + '-' + ele
+  return ast_file
 
 def get_compile_unit(source_file):
   global compile_unit
   if not source_file in compile_unit:
+    output = "the size of compile_unit set: %d\n" % len(compile_unit)
+    sys.stderr.write(output)
+    if(len(recent_unit)==15):
+      unrecent = recent_unit.popleft()
+      output = "delete compile_unit: %s\n" % unrecent
+      sys.stderr.write(output)
+      del compile_unit[unrecent]
+    output = "read ast file: %s ..." % source_file
+    sys.stderr.write(output + '\n')
+    ast_file = convert_to_ast(source_file)
+    if(os.access(ast_file, os.F_OK)):
+      try:
+        compile_unit[source_file] = clang_index.read(ast_file)
+        sys.stderr.write('reading done\n')
+        recent_unit.append(source_file)
+        return compile_unit[source_file]
+      except:
+        output = "an error occurred when reading ast and try parsing: %s\n" % e
+        sys.stderr.write(output)
+    sys.stderr.write("reading ast failed and try parsing\n")
     try:
       output = "parsing source file: %s ..." % source_file
       sys.stderr.write(output + '\n')
@@ -52,15 +81,23 @@ def get_compile_unit(source_file):
         args = clang_args[source_file]
       elif(source_file[-2:] == '.h'):
         args = h_args
+      elif(source_file[-4:] == '.inc'):
+        return None
       else:
         output = "clang_args not found: %s ..." % source_file
         sys.stderr.write(output + '\n')
         os.abort()
-      compile_unit[source_file] = clang_index.parse(source_file, args, options = clang.cindex.TranslationUnit.PARSE_DETAILED_PROCESSING_RECORD)
+      compile_unit[source_file] = clang_index.parse(source_file, args, options =   clang.cindex.TranslationUnit.PARSE_DETAILED_PROCESSING_RECORD)
       sys.stderr.write('parsing done\n')
+      assert(not os.access(ast_file, os.F_OK))
+      compile_unit[source_file].save(ast_file)
     except clang.cindex.TranslationUnitLoadError as e:
-      print ("an error occurred when creating compile unit: %s" % e)
+      output = "an error occurred when creating compile unit: %s\n" % e
+      sys.stderr.write(output)      
       compile_unit[source_file] = None
+  else:
+    recent_unit.remove(source_file)
+  recent_unit.append(source_file)
   return compile_unit[source_file]
 
 def get_cursor(file_name, line, column):
@@ -110,14 +147,14 @@ def print_cursor_children(cursor, level):
   if cursor.referenced is not None:
     ref_spelling = cursor.referenced.spelling
     ref_kind = cursor.referenced.kind
-  print "%s - %s - %s - ref: %s - %s" % (cursor.spelling, cursor.displayname, cursor.kind, ref_spelling, ref_kind)
+  print "%s - %s - %s - %d, %d ~ %d, %d - ref: %s - %s" % (cursor.spelling, cursor.displayname, cursor.kind, cursor.extent.start.line, cursor.extent.start.column, cursor.extent.end.line, cursor.extent.end.column, ref_spelling, ref_kind)
   for child in cursor.get_children():
     print_cursor_children(child, level+1)
 
 def parse_indirect_branch(cursor):
-  IndirectBrKind = enum.Enum('IndirectBrKind', 'SWITCH VIRCALL CALLBACK VIRDESTRUCT UNKNOWN')
+  IndirectBrKind = enum.Enum('IndirectBrKind', 'SWITCH VIRCALL CALLBACK VIRDESTRUCT UNKNOWN UNSUPPORTTED')
   # pdb.set_trace()
-  if cursor.kind is clang.cindex.CursorKind.RETURN_STMT or cursor.kind is clang.cindex.CursorKind.DECL_STMT or cursor.kind is clang.cindex.CursorKind.VAR_DECL or cursor.kind is clang.cindex.CursorKind.UNARY_OPERATOR or cursor.kind is clang.cindex.CursorKind.COMPOUND_STMT or cursor.kind is clang.cindex.CursorKind.UNEXPOSED_EXPR or cursor.kind is clang.cindex.CursorKind.IF_STMT:
+  if cursor.kind is clang.cindex.CursorKind.RETURN_STMT or cursor.kind is clang.cindex.CursorKind.DECL_STMT or cursor.kind is clang.cindex.CursorKind.VAR_DECL or cursor.kind is clang.cindex.CursorKind.UNARY_OPERATOR or cursor.kind is clang.cindex.CursorKind.COMPOUND_STMT or cursor.kind is clang.cindex.CursorKind.UNEXPOSED_EXPR or cursor.kind is clang.cindex.CursorKind.IF_STMT or cursor.kind is clang.cindex.CursorKind.BINARY_OPERATOR or cursor.kind is clang.cindex.CursorKind.CONDITIONAL_OPERATOR or cursor.kind is clang.cindex.CursorKind.PAREN_EXPR or cursor.kind is clang.cindex.CursorKind.CASE_STMT or cursor.kind is clang.cindex.CursorKind.CXX_REINTERPRET_CAST_EXPR or cursor.kind is clang.cindex.CursorKind.MEMBER_REF_EXPR:
     ret = False
     for child in cursor.get_children():
       if parse_indirect_branch(child) is True:
@@ -126,7 +163,7 @@ def parse_indirect_branch(cursor):
   elif cursor.kind is clang.cindex.CursorKind.SWITCH_STMT:
     # print "indirect branch kind: %s" % IndirectBrKind.SWITCH
     return True
-  elif cursor.kind is clang.cindex.CursorKind.CALL_EXPR and cursor.referenced is not None and (cursor.referenced.kind is clang.cindex.CursorKind.VAR_DECL or cursor.referenced.kind is clang.cindex.CursorKind.FIELD_DECL):
+  elif cursor.kind is clang.cindex.CursorKind.CALL_EXPR and cursor.referenced is not None and (cursor.referenced.kind is clang.cindex.CursorKind.VAR_DECL or cursor.referenced.kind is clang.cindex.CursorKind.FIELD_DECL or cursor.referenced.kind is clang.cindex.CursorKind.PARM_DECL):
     output = "indirect branch kind: %s" % IndirectBrKind.CALLBACK
     sys.stderr.write(output + '\n')
     return True
@@ -134,14 +171,34 @@ def parse_indirect_branch(cursor):
     output = "indirect branch kind: %s" % IndirectBrKind.VIRCALL
     sys.stderr.write(output + '\n')
     return True
+  elif cursor.kind is clang.cindex.CursorKind.CALL_EXPR:
+    ret = False
+    for child in cursor.get_children():
+      if parse_indirect_branch(child) is True:
+        ret = True
+    return ret
   elif cursor.kind is clang.cindex.CursorKind.CXX_DELETE_EXPR:
     output = "indirect branch kind: %s" % IndirectBrKind.VIRDESTRUCT
     sys.stderr.write(output + '\n')
     return True
-  else:
-    output = "indirect branch kind: %s" % IndirectBrKind.UNKNOWN
+  elif cursor.kind is clang.cindex.CursorKind.CXX_METHOD:
+    ret = False
+    for child in cursor.get_children():
+      if child.kind is clang.cindex.CursorKind.COMPOUND_STMT:
+        for child2 in child.get_children():
+          if child2.kind is clang.cindex.CursorKind.RETURN_STMT:
+            for child3 in child2.get_children():
+              if parse_indirect_branch(child3) is True:
+                ret = True
+    return ret
+  elif cursor.kind is clang.cindex.CursorKind.NO_DECL_FOUND or cursor.kind is clang.cindex.CursorKind.MACRO_INSTANTIATION or cursor.kind is clang.cindex.CursorKind.CLASS_DECL:
+    output = "indirect branch kind: %s" % IndirectBrKind.UNSUPPORTTED
     sys.stderr.write(output + '\n')
-    print "indirect branch kind: %s" % IndirectBrKind.UNKNOWN
+    return True
+  else:
+    # output = "indirect branch kind: %s" % IndirectBrKind.UNKNOWN
+    # sys.stderr.write(output + '\n')
+    # print "indirect branch kind: %s" % IndirectBrKind.UNKNOWN
     return False
 
 class AwsParse(object):
@@ -161,4 +218,6 @@ class AwsParse(object):
       # sys.stderr.flush()
       if parse_indirect_branch(cursor) is True:
         ret = True
+      if ret is False:
+        print "the result of parsing cursor is IndirectBrKind.UNKNOWN\n\t%s: %d, %d - %d, %d" % (file_name, cursor.extent.start.line, cursor.extent.start.column, cursor.extent.end.line, cursor.extent.end.column)
     return ret
